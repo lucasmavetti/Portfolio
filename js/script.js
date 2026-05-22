c0283c41e7b37dcf31a9cad3118606a2541ceff6
lucasmavetti <lucas.fernandes.dearaujo02@gmail.com>
2026-05-21 23:06:37 -0300

    tentando achar a cor

--- js/script.js
+++ js/script.js
@@ -1,28 +1,28 @@
 // Efeito de digitação
-function escreverLetra(elemento) {
-  function ativaLetra(elemento) {
-    const arrTexto = elemento.innerHTML.split("");
-    elemento.innerHTML = "";
-    arrTexto.forEach((letra, i) => {
-      setTimeout(() => {
-        elemento.innerHTML += letra;
-      }, 75 * i);
-    });
-  }
+function ativaLetra(elemento) {
+  const arrTexto = elemento.innerHTML.split("");
+  elemento.innerHTML = "";
+  arrTexto.forEach((letra, i) => {
+    setTimeout(() => {
+      elemento.innerHTML += letra;
+    }, 75 * i);
+  });
+}
 
+function escreverLetra(elemento) {
   const titulo = document.querySelector(".digitando");
   ativaLetra(titulo);
 }
 
 // Menu mobile
-const btnMenu = document.querySelector('.bi-three-dots-vertical, .bi-x');
-const iconMenu = document.querySelector('header i');
-const menuMobile = document.querySelector('.menu_mobile');
+const btnMenu = document.querySelector(".bi-three-dots-vertical, .bi-x");
+const iconMenu = document.querySelector("header i");
+const menuMobile = document.querySelector(".menu_mobile");
 
 if (iconMenu && menuMobile) {
-  iconMenu.addEventListener('click', () => {
-    menuMobile.classList.toggle('aberto');
-    iconMenu.classList.toggle('bi-three-dots-vertical');
-    iconMenu.classList.toggle('bi-x');
+  iconMenu.addEventListener("click", () => {
+    menuMobile.classList.toggle("aberto");
+    iconMenu.classList.toggle("bi-three-dots-vertical");
+    iconMenu.classList.toggle("bi-x");
   });
 }
